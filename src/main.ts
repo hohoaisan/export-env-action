730e067232a54d978fd87cd3c1fe31e1b2088c27
hoaisan <hohoaisan@gmail.com>
2023-01-05 14:23:03 +0700

    Feature/secret value (#3)
    * feat: add secret option to mask env value as secret
    
    * feat: README.MD and action.yml add secret input

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,22 @@ import * as dotenvExpand from "dotenv-expand";
 import inputs from "./inputs";
 import * as core from "@actions/core";
 
+const applyResultFunc = (name: string, value: any) => {
+    if (inputs.secret) {
+        core.setSecret(value)
+    }
+    if (inputs.export) {
+        core.exportVariable(name, value)
+    }
+    else core.setOutput(name, value);
+}
+
 export function runImpl() {
     let vars = dotenv.parse(fs.readFileSync(inputs.envFile));
     if (inputs.expand || inputs.expandWithJobEnv) {
         // @ts-ignore
-        vars = dotenvExpand.expand({parsed: vars, ignoreProcessEnv: !inputs.expandWithJobEnv}).parsed;
+        vars = dotenvExpand.expand({ parsed: vars, ignoreProcessEnv: !inputs.expandWithJobEnv }).parsed;
     }
-    const applyResultFunc = inputs.export
-        ? core.exportVariable
-        : core.setOutput;
 
     Object.entries(vars).forEach(e => applyResultFunc(e[0], e[1]));
 }
